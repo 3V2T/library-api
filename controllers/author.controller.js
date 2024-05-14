b159bb1e6f604b78598c98413a632b7012f85256
hahuynhvan <hahuynhhuy159@gmail.com>
2024-05-14 08:44:47 +0700

    14/05

--- controllers/author.controller.js
+++ controllers/author.controller.js
@@ -1,42 +1,42 @@
-const Author = require("../models/author.model");
+const Author = require('../models/author.model')
 
 const getAll = async (req, res) => {
-  const name = req.query.name;
+  const name = req.query.name
   if (name) {
-    const authorList = await Author.getBySlug(name);
+    const authorList = await Author.getBySlug(name)
     if (authorList.length !== 0) {
-      return res.status(200).json({ author: authorList });
+      return res.status(200).json({ author: authorList })
     } else {
-      return res.status(404).json({ msg: "There is no author" });
+      return res.status(404).json({ msg: 'There is no author' })
     }
   } else {
-    const authorList = await Author.getAll();
+    const authorList = await Author.getAll()
     if (authorList.length !== 0) {
-      return res.status(200).json({ author: authorList });
+      return res.status(200).json({ author: authorList })
     } else {
-      return res.status(404).json({ msg: "There is no author" });
+      return res.status(404).json({ msg: 'There is no author' })
     }
   }
-};
+}
 
 const getById = async (req, res) => {
-  const { id } = req.params;
-  const author = await Author.getById(id);
+  const { id } = req.params
+  const author = await Author.getById(id)
   if (author.length === 0) {
-    return res.status(404).json({ msg: "There isn't any author match!" });
+    return res.status(404).json({ msg: "There isn't any author match!" })
   } else {
-    return res.status(200).json({ author });
+    return res.status(200).json({ author })
   }
-};
+}
 
 const getByName = async (req, res) => {
-  const { name } = req.query;
-  const author = await Author.getByName(name);
+  const { name } = req.query
+  const author = await Author.searchByKeyword(name)
   if (author.length === 0) {
-    return res.status(404).json({ msg: "There isn't any author match!" });
+    return res.status(404).json({ msg: "There isn't any author match!" })
   } else {
-    return res.status(200).json({ author });
+    return res.status(200).json({ author })
   }
-};
+}
 
-module.exports = { getAll, getById, getByName };
+module.exports = { getAll, getById, getByName }
--- models/author.model.js
+++ models/author.model.js
@@ -1,50 +1,61 @@
-const conn = require("../db");
+const conn = require('../db')
 
 const Author = {
   getAll: () => {
     return new Promise(async (resolve, reject) => {
       try {
-        const sql = "SELECT * FROM authors";
-        const [results] = await conn.query(sql);
-        resolve(results);
+        const sql = 'SELECT * FROM authors'
+        const [results] = await conn.query(sql)
+        resolve(results)
       } catch (err) {
-        reject(err);
+        reject(err)
       }
-    });
+    })
   },
   getBySlug: (author) => {
     return new Promise(async (resolve, reject) => {
       try {
-        const sql = "SELECT * FROM authors WHERE description = ?";
-        const [results] = await conn.query(sql, [author]);
-        resolve(results);
+        const sql = 'SELECT * FROM authors WHERE description = ?'
+        const [results] = await conn.query(sql, [author])
+        resolve(results)
       } catch (err) {
-        reject(err);
+        reject(err)
       }
-    });
+    })
   },
   getById: (id) => {
     return new Promise(async (resolve, reject) => {
       try {
-        const sql = "CALL getAuthorsbyid(?)";
-        const [results] = await conn.query(sql, [id]);
-        resolve(results[0]);
+        const sql = 'CALL getAuthorsbyid(?)'
+        const [results] = await conn.query(sql, [id])
+        resolve(results[0])
       } catch (err) {
-        reject(err);
+        reject(err)
       }
-    });
+    })
   },
   getByName: (name) => {
     return new Promise(async (resolve, reject) => {
       try {
-        const sql = "SELECT * FROM authors WHERE author = ?";
-        const [results] = await conn.query(sql, [name]);
-        resolve(results);
+        const sql = 'SELECT * FROM authors WHERE author = ?'
+        const [results] = await conn.query(sql, [name])
+        resolve(results)
       } catch (err) {
-        reject(err);
+        reject(err)
       }
-    });
+    })
   },
-};
+  searchByKeyword: (keyword) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const sql = 'SELECT * FROM authors WHERE author LIKE?'
+        const [results] = await conn.query(sql, [`%${keyword}%`])
+        resolve(results)
+      } catch (err) {
+        reject(err)
+      }
+    })
+  },
+}
 
-module.exports = Author;
+module.exports = Author
