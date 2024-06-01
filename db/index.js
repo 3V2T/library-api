81ed75046d9676d383aeb5d45723f294b2e87091
aycact <CT06N0159@actvn.edu.vn>
2024-06-01 12:28:36 +0700

    chore: Update database configuration and environment variables
    Update the database configuration in `db/index.js` and `dbconfig.js` to use environment variables for the host, user, password, and database. Also, add new environment variables in the `.env` file for `DB_USER`, `DB_DATABASE`, and `MYSQL_DATABASE`. Remove unnecessary lines from `.env` and `compose.yaml`.
    
    Update `.gitignore` to include new files for running on Docker and Cloudflare Tunnel.
    
    Delete `package-lock.json`.
    
    Fixes #123

--- db/index.js
+++ db/index.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 const dbconfig = require("../dbconfig");
 console.log(dbconfig);
 const conn = mysql.createPool({
-    host: 'localhost', 
-    user: 'root', 
-    password: 'van123456', 
-    database: 'newlibrary',
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS, 
+    database: process.env.DB_DATABASE,
 });
 
 
